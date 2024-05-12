f96b2bf4b704a157f6c927e349e46a7e2ac41f97
Rayhan <nm.rayhan.1650@gmail.com>
2024-05-12 16:50:59 +0600

    Container practice done

--- lib/main.dart
+++ lib/main.dart
@@ -62,144 +62,171 @@ class Home extends StatelessWidget {
               icon: const Icon(Icons.add))
         ],
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisSize: MainAxisSize.max,
-          children: [
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.teal,
-                foregroundColor: Colors.white,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8),
-                  side: const BorderSide(
-                    width: 2,
-                    color: Colors.red,
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.teal,
+                  foregroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                    side: const BorderSide(
+                      width: 2,
+                      color: Colors.red,
+                    ),
+                  ),
+                  textStyle: const TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.w700,
+                  ),
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 20,
+                    vertical: 15,
                   ),
                 ),
-                textStyle: const TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.w700,
-                ),
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 20,
-                  vertical: 15,
-                ),
+                onPressed: () {
+                  print('pressed');
+                },
+                child: const Text('Press here'),
               ),
-              onPressed: () {
-                print('pressed');
-              },
-              child: const Text('Press here'),
-            ),
-            TextButton(
-              style: TextButton.styleFrom(
-                foregroundColor: Colors.red,
-                textStyle: const TextStyle(
-                  fontSize: 20,
-                ),
-              ),
-              onPressed: () {},
-              child: const Text('Text Button'),
-            ),
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                Icons.add_circle,
-                color: Colors.red,
-              ),
-            ),
-            GestureDetector(
-              onTap: () {
-                print('on tap detected');
-              },
-              onDoubleTap: () {
-                print('on doubletap detected');
-              },
-              child: const Column(
-                children: [
-                  Text('abcd'),
-                  Icon(Icons.add_circle),
-                  Row(
-                    children: [Text('This is a row')],
-                  )
-                ],
-              ),
-            ),
-            InkWell(
-              splashColor: Colors.orange,
-              radius: 20,
-              borderRadius: BorderRadius.circular(30),
-              onTap: () {
-                print('on tap detected');
-              },
-              onDoubleTap: () {
-                print('on doubletap detected');
-              },
-              child: const Column(
-                children: [
-                  Text('abcd'),
-                  Icon(Icons.add_circle),
-                  Row(
-                    children: [Text('This is a row')],
-                  )
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextField(
-                style: const TextStyle(
-                  fontSize: 24,
-                  color: Colors.green,
-                ),
-                maxLength: 10,
-                decoration: InputDecoration(
-                  hintText: "Enter your email",
-                  hintStyle: const TextStyle(
+              TextButton(
+                style: TextButton.styleFrom(
+                  foregroundColor: Colors.red,
+                  textStyle: const TextStyle(
                     fontSize: 20,
-                    fontWeight: FontWeight.w300,
                   ),
-                  labelText: "Email",
-                  labelStyle: const TextStyle(),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(50),
-                    borderSide: const BorderSide(
-                      width: 4,
-                      color: Colors.red,
-                    ),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(50),
-                    borderSide: const BorderSide(
-                      width: 4,
-                      color: Colors.red,
-                    ),
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(50),
-                    borderSide: const BorderSide(
-                      width: 4,
-                      color: Colors.green,
-                    ),
-                  ),
-                  disabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(50),
-                    borderSide: const BorderSide(
-                      width: 4,
-                      color: Colors.grey,
-                    ),
-                  ),
-                  fillColor: Colors.white,
-                  filled: true,
-                  prefixIcon: const Icon(Icons.add),
-                  prefixIconColor: Colors.red,
-                  suffixIcon: const Icon(Icons.remove_red_eye),
+                ),
+                onPressed: () {},
+                child: const Text('Text Button'),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.add_circle,
+                  color: Colors.red,
                 ),
               ),
-            )
-          ],
+              GestureDetector(
+                onTap: () {
+                  print('on tap detected');
+                },
+                onDoubleTap: () {
+                  print('on doubletap detected');
+                },
+                child: const Column(
+                  children: [
+                    Text('abcd'),
+                    Icon(Icons.add_circle),
+                    Row(
+                      children: [Text('This is a row')],
+                    )
+                  ],
+                ),
+              ),
+              InkWell(
+                splashColor: Colors.orange,
+                radius: 20,
+                borderRadius: BorderRadius.circular(30),
+                onTap: () {
+                  print('on tap detected');
+                },
+                onDoubleTap: () {
+                  print('on doubletap detected');
+                },
+                child: const Column(
+                  children: [
+                    Text('abcd'),
+                    Icon(Icons.add_circle),
+                    Row(
+                      children: [Text('This is a row')],
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  style: const TextStyle(
+                    fontSize: 24,
+                    color: Colors.green,
+                  ),
+                  maxLength: 10,
+                  decoration: InputDecoration(
+                    hintText: "Enter your email",
+                    hintStyle: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w300,
+                    ),
+                    labelText: "Email",
+                    labelStyle: const TextStyle(),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(50),
+                      borderSide: const BorderSide(
+                        width: 4,
+                        color: Colors.red,
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(50),
+                      borderSide: const BorderSide(
+                        width: 4,
+                        color: Colors.red,
+                      ),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(50),
+                      borderSide: const BorderSide(
+                        width: 4,
+                        color: Colors.green,
+                      ),
+                    ),
+                    disabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(50),
+                      borderSide: const BorderSide(
+                        width: 4,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    fillColor: Colors.white,
+                    filled: true,
+                    prefixIcon: const Icon(Icons.add),
+                    prefixIconColor: Colors.red,
+                    suffixIcon: const Icon(Icons.remove_red_eye),
+                  ),
+                ),
+              ),
+              Container(
+                height: 100,
+                width: 100,
+                decoration: BoxDecoration(
+                    color: Colors.green,
+                    borderRadius: BorderRadius.circular(16),
+                    border: Border.all(
+                      color: Colors.black,
+                      width: 2,
+                    ),
+                    image: const DecorationImage(
+                      image: AssetImage('images/bird.jpg'),
+                      fit: BoxFit.cover,
+                    ),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 5,
+                        blurRadius: 7,
+                        offset: Offset(0, 3),
+                      ),
+                    ]),
+                alignment: Alignment.center,
+                child: const Text("Container"),
+              )
+            ],
+          ),
         ),
       ),
     );
