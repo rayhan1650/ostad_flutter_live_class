85d5aa7f5fef5e9f7b2e54b2559a3d56a041f3b8
Rayhan <nm.rayhan.1650@gmail.com>
2024-05-21 08:45:27 +0600

    gridview builder added

--- lib/main.dart
+++ lib/main.dart
@@ -30,65 +30,65 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // body: ListView(
-      //   children: [
-      //     Text("Hello Flutter"),
-      //     Text("Hello Flutter"),
-      //     Text("Hello Flutter"),
-      //     Text("Hello Flutter"),
-      //     Text("Hello Flutter"),
-      //     Text("Hello Flutter"),
-      //     Text("Hello Flutter"),
-      //     Text("Hello Flutter"),
-      //     Text("Hello Flutter"),
-      //     Text("Hello Flutter"),
-      //     Text("Hello Flutter"),
-      //     Text("Hello Flutter"),
-      //     Text("Hello Flutter"),
-      //     Text("Hello Flutter"),
-      //     Text("Hello Flutter"),
-      //     Text("Hello Flutter"),
-      //     Text("Hello Flutter"),
-      //     Text("Hello Flutter"),
-      //     Text("Hello Flutter"),
-      //     Text("Hello Flutter"),
-      //     Text("Hello Flutter"),
-      //     Text("Hello Flutter"),
-      //     Text("Hello Flutter"),
-      //     Text("Hello Flutter"),
-      //     Text("Hello Flutter"),
-      //     Text("Hello Flutter Bangladesh"),
-      //     Text("Hello Flutter"),
-      //     Text("Hello Flutter"),
-      //     Text("Hello Flutter"),
-      //     Text("Hello Flutter"),
-      //     Text("Hello Flutter"),
-      //     Text("Hello Flutter"),
-      //     Text("Hello Flutter"),
-      //     Text("Hello Flutter"),
-      //   ],
-      // ),
-      // body: ListView.builder(
-      //     itemCount: studentName.length,
-      //     itemBuilder: (context, index) {
-      //       return Text(studentName[index]);
-      //     }),
-      // body: ListView.separated(
-      //   itemCount: studentName.length,
-      //   itemBuilder: (context, index) {
-      //     return Text(studentName[index]);
-      //   },
-      //   separatorBuilder: (context, index) {
-      //     return const Divider(
-      //       height: 20,
-      //       thickness: 2,
-      //       indent: 8,
-      //       endIndent: 16,
-      //       color: Colors.red,
-      //     );
-      //   },
-      // ),
-      body: GridView(
+      /*body: ListView(
+        children: [
+          Text("Hello Flutter"),
+          Text("Hello Flutter"),
+          Text("Hello Flutter"),
+          Text("Hello Flutter"),
+          Text("Hello Flutter"),
+          Text("Hello Flutter"),
+          Text("Hello Flutter"),
+          Text("Hello Flutter"),
+          Text("Hello Flutter"),
+          Text("Hello Flutter"),
+          Text("Hello Flutter"),
+          Text("Hello Flutter"),
+          Text("Hello Flutter"),
+          Text("Hello Flutter"),
+          Text("Hello Flutter"),
+          Text("Hello Flutter"),
+          Text("Hello Flutter"),
+          Text("Hello Flutter"),
+          Text("Hello Flutter"),
+          Text("Hello Flutter"),
+          Text("Hello Flutter"),
+          Text("Hello Flutter"),
+          Text("Hello Flutter"),
+          Text("Hello Flutter"),
+          Text("Hello Flutter"),
+          Text("Hello Flutter Bangladesh"),
+          Text("Hello Flutter"),
+          Text("Hello Flutter"),
+          Text("Hello Flutter"),
+          Text("Hello Flutter"),
+          Text("Hello Flutter"),
+          Text("Hello Flutter"),
+          Text("Hello Flutter"),
+          Text("Hello Flutter"),
+        ],
+      ),*/
+      /*body: ListView.builder(
+          itemCount: studentName.length,
+          itemBuilder: (context, index) {
+            return Text(studentName[index]);
+          }),
+      body: ListView.separated(
+        itemCount: studentName.length,
+        itemBuilder: (context, index) {
+          return Text(studentName[index]);
+        },
+        separatorBuilder: (context, index) {
+          return const Divider(
+            height: 20,
+            thickness: 2,
+            indent: 8,
+            endIndent: 16,
+            color: Colors.red,
+          );
+        },
+      ),*/
+      /*body: GridView(
         gridDelegate:
             const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
         children: const [
@@ -97,7 +97,26 @@ class Home extends StatelessWidget {
           Text("Hello"),
           Text("Hello"),
         ],
-      ),
+      ),*/
+      body: GridView.builder(
+          itemCount: studentName.length,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 3,
+            mainAxisSpacing: 2,
+            crossAxisSpacing: 4,
+          ),
+          itemBuilder: (context, index) {
+            return Container(
+              color: Colors.grey.shade200,
+              child: Column(
+                children: [
+                  Text("Roll - ${index + 1}"),
+                  Text(studentName[index]),
+                ],
+              ),
+            );
+          }),
     );
   }
 }
