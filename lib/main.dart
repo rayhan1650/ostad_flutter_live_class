62792c8cdd72467032c1f3f8197d17e6893dea31
Rayhan <nm.rayhan.1650@gmail.com>
2024-05-22 12:12:42 +0600

    cupertino design added

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,43 @@
-import 'package:flutter/material.dart';
-
-import 'home.dart';
+import 'package:flutter/cupertino.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const IOSStyleApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class IOSStyleApp extends StatelessWidget {
+  const IOSStyleApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: "Navigation",
-      home: Home(),
+    return const CupertinoApp(
+      home: HomePage(),
     );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+        navigationBar: CupertinoNavigationBar(
+          middle: const Text("Home"),
+          trailing: CupertinoButton(
+            onPressed: () {},
+            child: const Icon(CupertinoIcons.add),
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const CupertinoActivityIndicator(
+                radius: 24,
+              ),
+              CupertinoSwitch(value: false, onChanged: (value) {})
+            ],
+          ),
+        ));
   }
 }
