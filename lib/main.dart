743f8768ff4409c1da1e0f1ae031daa5d4d05794
Rayhan <nm.rayhan.1650@gmail.com>
2024-05-26 12:24:02 +0600

    themeData practice done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+/*import 'package:flutter/cupertino.dart';
 
 void main() {
   runApp(const IOSStyleApp());
@@ -40,4 +40,87 @@ class HomePage extends StatelessWidget {
           ),
         ));
   }
+}*/
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(),
+      theme: ThemeData(
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.pink,
+            foregroundColor: Colors.white,
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+          ),
+        ),
+        inputDecorationTheme: InputDecorationTheme(
+          enabledBorder: OutlineInputBorder(),
+          focusedBorder:
+              OutlineInputBorder(borderRadius: BorderRadius.circular(50)),
+          errorBorder:
+              OutlineInputBorder(borderSide: BorderSide(color: Colors.red)),
+        ),
+        appBarTheme: SetTheme.appBarTheme,
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      themeMode: ThemeMode.dark,
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(onPressed: () {}, child: Text("Tap to edit")),
+            SizedBox(
+              height: 10,
+            ),
+            ElevatedButton(onPressed: () {}, child: Text("Tap to edit")),
+            TextButton(
+              onPressed: () {},
+              child: Text("Tap to edit"),
+              style: TextButton.styleFrom(
+                foregroundColor: Color(0xFFB80E0E),
+              ),
+            ),
+            TextField()
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SetTheme {
+  static AppBarTheme appBarTheme = const AppBarTheme(
+    backgroundColor: Colors.green,
+    elevation: 3,
+    titleTextStyle: TextStyle(
+      color: Colors.white,
+      fontWeight: FontWeight.bold,
+      fontSize: 20,
+    ),
+  );
 }
